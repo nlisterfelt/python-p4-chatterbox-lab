dfcd96a7f08fd33e2ca482138ac65e4c2629b05f
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-11-25 15:53:02 -0600

    work with the client and server side

--- server/app.py
+++ server/app.py
@@ -14,13 +14,38 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.all()]
+        return make_response(messages, 200)
+    elif request.method == 'POST':
+        form_data = request.get_json()
+        new_message = Message(
+            body = form_data['body'],
+            username = form_data['username']
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return make_response(new_message.to_dict(), 201)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    if request.method == 'PATCH':
+        form_data = request.get_json()
+        for attr in form_data:
+            setattr(message, attr, form_data.get(attr))
+        db.session.commit()
+        return make_response(message.to_dict(), 200)
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        response_body = {
+            "delete_successful": True,
+            "message": f"Message {id} successfully deleted."
+        }
+        return make_response(response_body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
